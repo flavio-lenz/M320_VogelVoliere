ded9e9e5074f9263a2f48969894c0286e0b8c2d4
Flavio Lenz <info@flavio-lenz.ch>
2022-12-14 11:35:05 +0100

    added Amsel added Amsel / Strauss in Program.cs

---
+++ VogelVoliere/Amsel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VogelVoliere
+{
+    public class Amsel : Vogel
+    {
+        public Amsel(string name) : base(name)
+        {
+        }
+    }
+}
--- VogelVoliere/Program.cs
+++ VogelVoliere/Program.cs
@@ -11,10 +11,14 @@ namespace VogelVoliere
             Vogel Vogel1 = new Vogel("Vogel 1");
             Vogel Vogel2 = new Vogel("Vogel 2");
             Vogel Vogel3 = new Vogel("Vogel 3");
+            Amsel Amsel1 = new Amsel("Amsel 1");
+            Strauss Strauss1 = new Strauss("Strauss 1");
 
             VogelVoliere1.AddVogel(Vogel1);
             VogelVoliere1.AddVogel(Vogel2);
             VogelVoliere1.AddVogel(Vogel3);
+            VogelVoliere1.AddVogel(Amsel1);
+            VogelVoliere1.AddVogel(Strauss1);
 
             VogelVoliere1.VogelFliegt();
         }
